d56e1f68ab34718ee4fc55aa4746f40c604943f3
David <davidmollera@hotmail.com>
2022-10-23 13:29:42 -0500

    solved the same problem but using push to add the selected values into an empty array, the conole logging that new array

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let grades = [ 'A', 'A+', 'Fail']
+let grades = [ 'A', 'A+', 'Fail', 'B', 'C']
+
+let goodGrades = []
 
 for ( let i = 0; i < grades.length; ++i) {
     if (grades [i] !== 'Fail'){
-        console.log( grades[i])
+        goodGrades.push (grades[i])
     }
 }
 
+console.log(goodGrades)
