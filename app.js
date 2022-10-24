6d3545b20a495b7b4d33b0d7325e7cdd2e1e239d
David <davidmollera@hotmail.com>
2022-10-23 19:53:09 -0500

    created a register function, that when called creates an object containing all of the user data, then pushes those values into the users array

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-let dollars = [1, 5, 10, 3]
-let cents = []
+let users = [];
 
-for (let i = 0; i < dollars.length; ++i) {
-    cents.push (dollars [i] * 100)
+function register(
+  username,
+  email,
+  password,
+  suscriptionStatus,
+  discordId,
+  lessonsCompleted
+) {
+    let user = {
+        username: username,
+        email: email,
+        password: password,
+        suscriptionStatus: suscriptionStatus,
+        discordId: discordId,
+        lessonsCompleted: lessonsCompleted
+    }
+    users.push(user)
 }
 
+register(
+  "molberda",
+  "davidmollera@hotmail.com",
+  "1234",
+  "VIP",
+  "Molberda#1234",
+  [0, 1, 2]
+  );
+  
+  register(
+    "damolber",
+    "danielmollera@hotmail.com",
+    "1234555",
+    "VIP",
+    "damolber#1234",
+    [0, 1, 2]
+    );
 
-console.log(cents)
+  console.log(users[0]);
