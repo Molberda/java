06f88a60df0b5dc9ee4e81e2eaa1cdca03c4d032
David <davidmollera@hotmail.com>
2022-10-24 23:42:51 -0500

    used a 'better practice' to acomplish the same goal of importing data from backend link and modifying html file with it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nameRef = document.querySelector(".name");
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-  response.json().then(data => {
-    nameRef.innerHTML = data.name
-  });
-});
+ return response.json()
+}).then(data => {
+   nameRef.innerHTML = data.name
+ });
